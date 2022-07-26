c46148e1b573a7ca258ac4f3c0183a99afa67d08
Lucas Araujo <lucasaraujo8186@gmail.com>
2022-07-26 17:04:00 -0300

    feat: track location hook

---
+++ src/hooks/useTrackLocation.ts
@@ -0,0 +1,78 @@
+import { useState, useRef, useEffect } from 'react';
+import Geolocation from 'react-native-geolocation-service';
+
+type Location = {
+  lat: number;
+  long: number;
+};
+
+/**
+ * This hook return object with a startTrack, stopTrack, isTracking, location, and checkLocationPermission
+ * function
+ *
+ * @param {number} [time=1000] - number = 1000
+ * @returns An object with the following properties:
+ */
+const useTrackLocation = (time: number = 1000) => {
+  const [location, setLocation] = useState<Location>({ lat: 0, long: 0 });
+  const [permission, setPermission] = useState<string>('undefined');
+  const [isTracking, setIsTracking] = useState(false);
+  const interval = useRef<ReturnType<typeof setInterval>>();
+
+  const checkLocationPermission = async () => {
+    const statusPermission = await Geolocation.requestAuthorization(
+      'whenInUse'
+    );
+    if (statusPermission === 'granted') {
+      setPermission('granted');
+    }
+  };
+
+  useEffect(() => {
+    if (permission !== 'granted') {
+      checkLocationPermission();
+    }
+  }, [permission]);
+
+  const startTrack = () => {
+    setIsTracking(true);
+    interval.current = setInterval(() => {
+      Geolocation.getCurrentPosition(
+        ({ coords: { latitude, longitude } }) => {
+          setLocation((prevLocation) => {
+            if (
+              prevLocation.lat !== latitude &&
+              prevLocation.long === longitude
+            ) {
+              return { lat: latitude, long: longitude };
+            }
+
+            return { lat: prevLocation.lat, long: prevLocation.long };
+          });
+        },
+        (error) => {
+          // See error code charts below.
+          console.log(error.code, error.message);
+        },
+        { enableHighAccuracy: true }
+      );
+    }, time);
+  };
+
+  const stopTrack = () => {
+    setIsTracking(false);
+    setLocation({ lat: 0, long: 0 });
+    Geolocation.stopObserving();
+    clearInterval(interval.current);
+  };
+
+  return {
+    startTrack,
+    stopTrack,
+    isTracking,
+    location,
+    checkLocationPermission,
+  };
+};
+
+export default useTrackLocation;
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,31 +1,47 @@
 import { Button } from '@app/components';
+import { useTrackLocation } from '@app/hooks';
 import { navigationService } from '@app/services';
 import { Text, Box } from '@app/theme';
 import React from 'react';
 
 const screens = ['PanGesture', 'Skia'];
 
-const Home = () => (
-  <>
-    <Box flex={1}>
-      <Box
-        width="100%"
-        alignSelf="center"
-        justifyContent="center"
-        alignItems="center"
-        height={200}
-      >
-        <Text textAlign="center">Welcome!</Text>
+const Home = () => {
+  const { location, startTrack, stopTrack } = useTrackLocation(500);
+
+  return (
+    <>
+      <Box flex={1}>
+        <Box
+          width="100%"
+          alignSelf="center"
+          justifyContent="center"
+          alignItems="center"
+          height={200}
+        >
+          <Text fontSize={40} textAlign="center">
+            Welcome!
+          </Text>
+          <Text fontWeight="700" textAlign="center">
+            Latitude: {location.lat}
+          </Text>
+          <Text textAlign="center">Longitude: {location.long}</Text>
+        </Box>
       </Box>
-    </Box>
-    {screens.map((screen) => (
       <Button
-        label={screen}
-        onPress={() => navigationService.navigate(screen)}
+        label={location.lat === 0 ? 'Start' : 'Stop'}
+        onPress={() => (location.lat === 0 ? startTrack() : stopTrack())}
         primary
       />
-    ))}
-  </>
-);
+      {screens.map((screen) => (
+        <Button
+          label={screen}
+          onPress={() => navigationService.navigate(screen)}
+          primary
+        />
+      ))}
+    </>
+  );
+};
 
 export default Home;
